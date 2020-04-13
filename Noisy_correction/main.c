093c1464e1c950ee53bd093d82a3770a69a23f03
thomas-ux <thomas.manteaux@epfl.ch>
2020-04-13 10:09:56 +0200

    Camera

--- Noisy_correction/main.c
+++ Noisy_correction/main.c
@@ -12,7 +12,11 @@
 #include <audio/microphone.h>
 
 #include <arm_math.h>
+#include "sensors/VL53L0X/VL53L0X.h"
+#include "process_image.h"
 
+#define DISTANCE_MAX		8190
+#define TOUR				1300
 
 static void serial_start(void)
 {
@@ -26,21 +30,18 @@ static void serial_start(void)
 	sdStart(&SD3, &ser_cfg); // UART3.
 }
 
-static void timer12_start(void){
-    //General Purpose Timer configuration   
-    //timer 12 is a 16 bit timer so we can measure time
-    //to about 65ms with a 1Mhz counter
-    static const GPTConfig gpt12cfg = {
-        1000000,        /* 1MHz timer clock in order to measure uS.*/
-        NULL,           /* Timer callback.*/
-        0,
-        0
-    };
-
-    gptStart(&GPTD12, &gpt12cfg);
-    //let the timer count to max value
-    gptStartContinuous(&GPTD12, 0xFFFF);
+void SendUint8ToComputer(uint8_t* data, uint16_t size)
+{
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)"START", 5);
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)&size, sizeof(uint16_t));
+	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
 }
+
+int32_t return_cible(int32_t compteur, int32_t cible);
+
+void position_cible(int32_t cible, int32_t compteur);
+
+static uint16_t mesure = DISTANCE_MAX;
 
 int main(void)
 {
@@ -53,16 +54,84 @@ int main(void)
     serial_start();
     //starts the USB communication
     usb_start();
-    //starts timer 12
-    timer12_start();
     //inits the motors
     motors_init();
 
+    //VL53L0X_start();
+    dcmi_start();
+    	po8030_start();
+    	process_image_start();
+
+    	int32_t compteur = 0;
+    	int32_t cible = 0;
+
     while(1)
     {
+    		//compteur = right_motor_get_pos();
 
+
+    		//cible = return_cible(compteur, cible);
+    			//chprintf((BaseSequentialStream *)&SD3, "mesure = %d mm cible %d = \n", mesure, cible);
+
+    		//position_cible(cible, compteur);
+    	right_motor_set_pos(0);
+    	if(get_action())
+    	{
+    		while(right_motor_get_pos()<650)
+    		{
+    			right_motor_set_speed(400);
+    			left_motor_set_speed(-400);
+    		}
+    	}
+    	else
+    	{
+    		right_motor_set_speed(0);
+    		left_motor_set_speed(0);
+    	}
+    	   chThdSleepMilliseconds(1000);
     }
+}
 
+
+
+int32_t return_cible(int32_t compteur, int32_t cible)
+{
+	if(compteur<TOUR)
+	{
+	    	right_motor_set_speed(150);
+	    	left_motor_set_speed(-150);
+
+	    	if(VL53L0X_get_dist_mm() < mesure && VL53L0X_get_dist_mm() > 0)
+	    	{
+	    		mesure = VL53L0X_get_dist_mm();
+	    		cible = compteur;
+	    	}
+	    //	return 0;
+	}
+ 	else if(compteur==TOUR)
+	{
+ 		right_motor_set_speed(0);
+	    left_motor_set_speed(0);
+	}
+
+	return cible;
+}
+
+void position_cible(int32_t cible, int32_t compteur)
+{
+	int32_t direction = (compteur - cible);
+	chprintf((BaseSequentialStream *)&SD3, "direction = %d mm cible %d = \n", compteur, cible);
+	if(direction >= (TOUR/2))
+	{
+		right_motor_set_pos(cible);
+		left_motor_set_pos(-cible);
+	}
+	else
+	{
+		right_motor_set_pos(-cible);
+		left_motor_set_pos(cible);
+	}
+}
 
 #define STACK_CHK_GUARD 0xe2dee396
 uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
