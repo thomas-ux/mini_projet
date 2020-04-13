0b41d6fe86768729ded9be5a235fd5c22796da3b
Martin <martin.lentz@epfl.ch>
2020-04-13 16:21:18 +0200

    Direction cible maj

--- Noisy_correction/main.c
+++ Noisy_correction/main.c
@@ -131,15 +131,15 @@ void direction_cible(int32_t cible)
 {
 	left_motor_set_pos(0);
 	if(cible >= (TOUR/2)){
-		while((-left_motor_get_pos())<(TOUR-cible)){
-			right_motor_set_speed(150);
-			left_motor_set_speed(-150);
+		while(left_motor_get_pos()<(TOUR-cible)){
+			right_motor_set_speed(-150);
+			left_motor_set_speed(150);
 		}
 	}
 	else if(cible < (TOUR/2)){
-		while(left_motor_get_pos()<cible){
-			right_motor_set_speed(-150);
-			left_motor_set_speed(150);
+		while((-left_motor_get_pos())<cible){
+			right_motor_set_speed(150);
+			left_motor_set_speed(-150);
 		}
 	}
 	right_motor_set_speed(0);
