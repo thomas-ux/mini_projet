efc7881d96fe7e80a27d1a53f348bd3502b781bc
Martin <martin.lentz@epfl.ch>
2020-04-14 11:37:31 +0200

    Structure etat_cible added

--- Noisy_correction/main.c
+++ Noisy_correction/main.c
@@ -15,8 +15,9 @@
 #include "sensors/VL53L0X/VL53L0X.h"
 #include "process_image.h"
 
-#define DISTANCE_MAX		8190
+#define DISTANCE_MAX		1000 //en mm (au max une cible peut se trouver à 1m de distance)
 #define TOUR				1300
+#define NB_CIBLES			10
 
 static void serial_start(void)
 {
@@ -37,13 +38,16 @@ void SendUint8ToComputer(uint8_t* data, uint16_t size)
 	chSequentialStreamWrite((BaseSequentialStream *)&SD3, (uint8_t*)data, size);
 }
 
-int32_t return_cible(int32_t compteur, int32_t cible, bool target);
 
-void direction_cible(int32_t cible);
+void init_tab_cible(void);
+void tri_croissant(void);
+void return_cible(int32_t compteur, bool target);
+void direction_cible(uint8_t num_cible);
 
-static uint16_t mesure = DISTANCE_MAX;
 
-
+//static uint16_t mesure = DISTANCE_MAX;
+static uint8_t indice = 0;
+static etat_cible tab_cible[NB_CIBLES] = {0};
 
 int main(void)
 {
@@ -65,8 +69,9 @@ int main(void)
     process_image_start();
 
     int32_t compteur = 0;
-    int32_t cible = 0;
+    uint8_t num_cible = 0;
     bool target = 0; //indique si on a une cible acquise ou non
+    init_tab_cible();
 
     while(1)
     {
@@ -74,13 +79,13 @@ int main(void)
     	compteur = right_motor_get_pos();
 
 
-    	cible = return_cible(compteur, cible, target);
-    	chprintf((BaseSequentialStream *)&SD3, "mesure = %d mm cible %d = \n", mesure, cible);
+    	return_cible(compteur, target);
+    	//chprintf((BaseSequentialStream *)&SD3, "mesure = %d mm cible %d = \n", mesure, cible);
 
     	if(compteur==TOUR)
     	{
     		target=1;
-    		direction_cible(cible);
+    		direction_cible(num_cible);
     	}
 
 
@@ -104,42 +109,76 @@ int main(void)
     }
 }
 
+void init_tab_cible(void)
+{
+	for(int i=0; i<NB_CIBLES; i++)
+		tab_cible[i].distance=DISTANCE_MAX;
+}
+
+void tri_croissant(void)
+{
+	uint16_t min = DISTANCE_MAX;
+	uint8_t i_min = 0;
+	int32_t position = 0;
+	for(int j=0; j<NB_CIBLES-1; j++)
+	{
+		for(int i=j; i<NB_CIBLES-1; i++)
+		{
+			if(tab_cible[i].distance < min)
+			{
+				min = tab_cible[i].distance;
+				position = tab_cible[i].orientation;
+				i_min = i;
+			}
+		}
+		tab_cible[i_min].distance = tab_cible[j].distance;
+		tab_cible[i_min].orientation = tab_cible[j].orientation;
+		tab_cible[j].distance = min;
+		tab_cible[j].orientation = position;
+		min = DISTANCE_MAX;
+	}
+}
 
 
-int32_t return_cible(int32_t compteur, int32_t cible, bool target)
+void return_cible(int32_t compteur, bool target)
 {
 	if(compteur<TOUR && !target)
 	{
 	    	right_motor_set_speed(150);
 	    	left_motor_set_speed(-150);
 
-	    	if(VL53L0X_get_dist_mm() < mesure && VL53L0X_get_dist_mm() > 0)
+	    	if(VL53L0X_get_dist_mm() < DISTANCE_MAX && VL53L0X_get_dist_mm() > 0)
 	    	{
-	    		mesure = VL53L0X_get_dist_mm();
-	    		cible = compteur;
+	    		tab_cible[indice].distance = VL53L0X_get_dist_mm();
+	    		tab_cible[indice].orientation = compteur;
+	    		if(indice>0)
+	    			indice--;
+	    		if(indice==0)
+	    		{
+	    			tri_croissant(); //pour qu'on écrase seulement les cibles les plus loin si il y a plus de 10 cibles
+	    			indice=9;
+	    		}
 	    	}
-	    //	return 0;
 	}
  	else if(compteur==TOUR)
 	{
  		right_motor_set_speed(0);
 	    left_motor_set_speed(0);
 	}
-
-	return cible;
 }
 
-void direction_cible(int32_t cible)
+void direction_cible(uint8_t num_cible)
 {
+	tri_croissant(); //ordonne tableau dans ordre croissant
 	left_motor_set_pos(0);
-	if(cible >= (TOUR/2)){
-		while(left_motor_get_pos()<=(TOUR-cible)){
+	if(tab_cible[num_cible].orientation >= (TOUR/2)){
+		while(left_motor_get_pos()<=(TOUR-tab_cible[num_cible].orientation)){
 			right_motor_set_speed(-150);
 			left_motor_set_speed(150);
 		}
 	}
 	else{
-		while((-left_motor_get_pos())<=cible){
+		while((-left_motor_get_pos())<=tab_cible[num_cible].orientation){
 			right_motor_set_speed(150);
 			left_motor_set_speed(-150);
 		}
